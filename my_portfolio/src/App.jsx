c09a325fbdd411ccbf44418d7e016df6a7eae5fe
Chandani Atraye <chandaniatraye5010@gmail.com>
2026-06-08 23:25:23 +0530

    portfolio

---
+++ my_portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <h1>Chandani Atraye</h1>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
